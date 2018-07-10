0f3c28e7bf145b0dfe91dc4798795e530426f49b
Vishal Sharma <vsh1819@gmail.com>
2018-07-10 15:03:24 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,8 @@ class Application
  
   def call(env)
     resp = Rack::Response.new
-    if D
-    resp.write "Hello, World"
+    if Time.now < 12
+    resp.write "Good, Morning"
     resp.finish
   end
  
